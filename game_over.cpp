b60f164caf68b8d8e1600ecaea3292b7ed839d02
kevin <kdelamo94@gmail.com>
2019-01-07 15:05:26 -0500

    Game Over using the "using" directive for brevity.

--- game_over.cpp
+++ game_over.cpp
@@ -1,8 +1,10 @@
 //Game Over
 //A first C++ program
 #include <iostream>
+using namespace std;
+
 
 int main(){
-  std::cout << "Game Over!" << std::endl;
+  cout << "Game Over!" << endl;
   return 0;
 }
